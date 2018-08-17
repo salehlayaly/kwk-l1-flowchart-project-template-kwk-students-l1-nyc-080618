144a69d6bee989d4e7cdea562cb0eaaff423e1a4
salehlayaly <lsaleh@hcstonline.org>
2018-08-17 15:20:53 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,31 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get "/" do
+    erb :index
+  end
+  
+  get "/help" do
+    erb :help
+  end
+  
+  get "/about" do
+    erb :about
+  end
+  
+  get '/result' do
+    @zipcode = params[:zipcode]
+    @locations =
+      {
+        'central harlem' => ["http://www.harlemgrown.org/", "https://www.harlemonestop.com/organization/1310/harlem-dowling-west-side-center-for-children-and-family-services", "http://www.coalitionforthehomeless.org/resources/salvation-army-east-harlem-community-center/"],
+        'chelsea' => ["https://www.foodpantries.org/ci/ny-chelsea", "https://www.volunteermatch.org/search/opp2427690.jsp", "https://volunteermatch.networkforgood.com/?code=Footer"],
+        'gramercy park/murry hill' => ['https://www.foodbanknyc.org/donate-food/','http://www.foodbanknyc.org/volunteer/', 'https://secure3.convio.net/fbnyc/site/Donation2?df_id=7464&mfc_pref=T&7464.donation=form1&_ga=2.210780370.694228225.1534401644-1055037965.1534401644'],
+        'soho greenwich' => ['https://www.cityharvest.org/programs/donate-food/','https://www.sohostudiosmiami.com/our-blog/2017/9/27/how-to-create-an-event-that-helps-support-disaster-relief', 'https://giving.cityharvest.org/donate-now'],
+        'lower manhattan' => ['https://www.homelessshelterdirectory.org/cgi-bin/id/cityfoodbanks.cgi?city=Manhattan&state=NY','https://www.nycservice.org/search/','https://donatenow.networkforgood.org/nycmedics'],
+        'upper east side' => ['https://www.foodbanknyc.org/donate-food/','https://www1.nyc.gov/site/em/volunteer/nyc-cert.page','https://secure3.convio.net/fbnyc/site/Donation2?df_id=5844&mfc_pref=T&5844.donation=form1&recurring=1&_ga=2.47253479.1983913834.1534424903-118813911.1534424903'],
+      }
+    erb :result
+  end
 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,65 @@
-class Question
+# class Question
+#   if (zipcode == "10026" || zipcode == "10027" || zipcode =="10030" ||zipcode == "10037" || zipcode == "10039")
+# neighborhood = "Central Harlem"
 
-end
+# # puts "organization page for this area Harlem"
+# elsif (zipcode == "10001" || zipcode == "10011"|| zipcode == "10018" ||zipcode == "10019" || zipcode == "10036")
+# neighborhood = "Chelsea"
+
+
+# elsif (zipcode == "10010" || zipcode == "10016"|| zipcode == "10017" ||zipcode == "10022")
+# neighborhood = "Gramercy Park/ Murray Hill"
+# # puts "organization page for Gramercy Park and Murray Hill"
+# elsif (zipcode == "10012" ||  zipcode == "10013" || zipcode == "10014")
+# neighborhood = "Soho/ Greenwich Village"
+
+# # puts organization page for Soho & Greenwich Village
+# elsif (zipcode == "10004" || zipcode == "10005" || zipcode == "10006" || zipcode == "10007" || zipcode == "10038" || zipcode == "10280")
+# neighborhood = "Lower Manhattan"
+# # puts organization page for Lower Manhattan
+# elsif (zipcode == "10021" || zipcode == "10028" || zipcode == "10044" || zipcode == "10065" || zipcode == "10075" || zipcode == "10128")
+# neighborhood = "Upper East Side"
+
+# end
+
+
+# central_harlem_hash = {"Food": ["http://www.harlemgrown.org/"], 
+# "Time":["https://www.harlemonestop.com/organization/1310/harlem-dowling-west-side-center-for-children-and-family-services"], "Donations": ["http://www.coalitionforthehomeless.org/resources/salvation-army-east-harlem-community-center/"]}
+
+# chelsea_hash = {"Food": ["https://www.foodpantries.org/ci/ny-chelsea"],
+# "Time": ["https://www.volunteermatch.org/search/opp2427690.jsp"], "Donations": ["https://volunteermatch.networkforgood.com/?code=Footer"]}
+
+# gramercy_park_murry_hill_hash = {"Food": ["https://www.foodbanknyc.org/donate-food/"],
+# "Time": ["http://www.foodbanknyc.org/volunteer/"], "Donations":["https://secure3.convio.net/fbnyc/site/Donation2?df_id=7464&mfc_pref=T&7464.donation=form1&_ga=2.210780370.694228225.1534401644-1055037965.1534401644"]}
+
+# soho_greenwich_hash = {"Food": ["https://www.cityharvest.org/programs/donate-food/"], "Time": ["https://www.sohostudiosmiami.com/our-blog/2017/9/27/how-to-create-an-event-that-helps-support-disaster-relief"], "Donations": ["https://giving.cityharvest.org/donate-now"]}
+# lower_manhattan_hash = {"Food": ["https://www.homelessshelterdirectory.org/cgi-bin/id/cityfoodbanks.cgi?city=Manhattan&state=NY"], "Time": ["https://www.nycservice.org/search/"], "Donations": ["https://donatenow.networkforgood.org/nycmedics"]}
+
+# upper_east_side_hash = {"Food": ["https://www.foodbanknyc.org/donate-food/"], "Time": ["https://www1.nyc.gov/site/em/volunteer/nyc-cert.page"], "Donations": ["https://secure3.convio.net/fbnyc/site/Donation2?df_id=5844&mfc_pref=T&5844.donation=form1&recurring=1&_ga=2.47253479.1983913834.1534424903-118813911.1534424903"]}
+
+# def iterate_through_hash(hash)
+# hash.each do |key, value|
+# puts "If you would like to work in #{key} you can try the following links:"
+#   value.each do |link|
+#   puts link
+#   end
+# end
+# end
+
+
+
+# if neighborhood == "Central Harlem"
+# iterate_through_hash(central_harlem_hash)
+# elsif neighborhood == "Chelsea"
+# iterate_through_hash(chelsea_hash)
+# elsif neighborhood == "Gramercy Park_Murray Hill"
+# iterate_through_hash(gramercy_park_murry_hill_hash)
+# elsif neighborhood == "Soho_Greenwich Village"
+# iterate_through_hash(soho_greenwich_hash)
+# elsif neighborhood == "Lower Manhattan"
+# iterate_through_hash(lower_manhattan_hash)
+# elsif neighborhood == "Upper East Side"
+# iterate_through_hash(upper_east_side_hash)
+# end
+
+# end
